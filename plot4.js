d45b14a47e891b7fd8c3ddec9fdc0d6c805635ce
scottsfarley <scottsfarley@berkeley.edu>
2014-10-24 15:44:55 -0700

    almost working

--- plot4.js
+++ plot4.js
@@ -1,8 +1,9 @@
-var plotWidth = 1000;
+var plotWidth =500;
 var curveHeight = 800;
 var nameOffset = 150;
 var axisWidth = 100;
-var PlotHeight  = nameOffset + curveHeight;
+var plotHeight  = nameOffset + curveHeight;
+var totalHeight = plotHeight + 200;
 var data = {}
 var names = {}
 var ids = []
@@ -11,8 +12,8 @@ var depthMinMax = [10000, 0]
 var active_taxa = []
 var depthScale;
 divWidth = $('#plot').width()
-plotWidth  = divWidth;
 console.log(plotWidth)
+active_names = []
 
 function setupSVG(height, width){
 	svg = d3.select('#plot').append('svg')
@@ -22,9 +23,8 @@ function setupSVG(height, width){
 }
 
 depthSVG = setupSVG(curveHeight, axisWidth).attr('id', 'depthAxisSvg')
-chronSVG = setupSVG(curveHeight, axisWidth)
-plotSVG = setupSVG(curveHeight, plotWidth)
-
+//chronSVG = setupSVG(curveHeight, axisWidth)
+plotSVG = setupSVG(curveHeight, plotWidth).attr('id', 'plotSvg')
 function getDepths(data){
 	depths = []
 	for (i in data){ //loops over multiple sets from different csvs
@@ -73,7 +73,7 @@ function loadData(csv){
 			.orient('left')
 		var depth = d3.select('#depthAxisSvg').append('g')
 			.attr('class', 'axis')
-			.attr('transform', 'translate(65, 0)')
+			.attr('transform', 'translate(95, 0)')
 			.call(depthAxis)
 		
 		//find names
@@ -104,46 +104,87 @@ function loadData(csv){
 function getValues(dataset, name){ //get the values for a certain taxon 
 	taxon = []
 	index = names[dataset].indexOf(name)
-	for (level in data[dataset]){
+	numlevels = data[dataset].length
+	level = 1 //gets rid of depth but not age
+	while (level < numlevels){
 		depth = data[dataset][level][0]
 		d = +data[dataset][level][index]
 		point = {'x': d, 'y': depth}
 		taxon.push(point)
+		level +=1
 	}
 	return taxon
 }
 		
 
 function makePlot(at){ //the central graph loop
+	d3.selectAll('.curve').remove()
 	var x_offset = 0;
 	plot = plotSVG;
 	for (t in at){
+		console.log('working at: ' + x_offset)
 		datafile = at[t]['datafile']
 		name = at[t]['Tname']
 		var tvals = getValues(datafile, name)
+		id = name.replace(" ", "_")
+		curve = plot.append('g')
+			.attr('class', 'curve')
+			.attr('id', id)
+		console.log(curve.attr('id'))
 		//put 45 degree rotated label on top
-		top_label = plot.append('text')
+		top_label = curve.append('text')
 			.attr('class', 'toplabel')
 			.attr('x', x_offset)
-			.attr('text-anchor', 'begin')
-			.attr('y', nameOffset -10)
-			.attr('transform', 'rotate(-45 ' + x_offset + ',' + nameOffset + ')')
+			.attr('y', nameOffset - 10)
+			.attr('transform', 'rotate(-45 ' + (x_offset + 20) + ',' +( nameOffset + -20) + ')')
 			.text(name)
 		//do scaling stuff
+		m = d3.max(tvals, function(d){ return d.x})
+		console.log('Max: ' + (m))
 		var taxScale = d3.scale.linear()
-			.domain([0, d3.max(tvals, function(d){ return d.x})])
-			.range([x_offset, (plotWidth-20)/2])
+			.domain([0, m])
+			.range([x_offset, (x_offset + (plotWidth-20)/at.length)])
+		console.log('Scaled' + taxScale(m))
 		pathvals = []
+		start = {'x': x_offset, 'y': nameOffset}
+		pathvals.push(start)
 		for (t in tvals){
 			x = tvals[t]['x']
 			scaledX = +taxScale(x)
 			y = tvals[t]['y']
 			depthY = +depthScale(y) // could accomodate scaing my chronology later on
 			point = {'x': scaledX, 'y': depthY} 
-			
+			pathvals.push(point)
 		}
+		end = {'x': x_offset, 'y':curveHeight}
+		pathvals.push(end)
+		//transform to svg path
+		var line = d3.svg.line()
+			.x(function (d) {return d.x})
+			.y(function(d){return d.y})
+			.interpolate('bundle')
+		path = line(pathvals)
+		
+		//append the path the curve group
+		var curve = curve.append('path')
+			.attr('d', path)
+			.attr('stroke', 'red')
+			.attr('fill', 'blue')
+			
+		//draw out the axis
+		var taxAxis = d3.svg.axis()
+			.scale(taxScale)
+			.orient('bottom')
+		var tax = curve.append('g')
+			//.attr('class', 'axis')
+			//.attr('transform', 'translate(' + x_offset + ',' + (curveHeight + 20) + ')')
+			//.call(taxAxis)
+		
+		x_offset += (plotWidth - 20)/at.length
 	}
 }
+
+	
 
 var dispatch = function(){
 	var input = this
@@ -152,8 +193,20 @@ var dispatch = function(){
 		f = $(this).data('file')
 		taxon = {'datafile': f, 'Tname' : name}
 		active_taxa.push(taxon)
+		active_names.push(name)
 		makePlot(active_taxa)
 	}else{
-		console.log('unchecked')
+		name = this.name
+		d3.selectAll('.curve').remove()
+		index = active_names.indexOf(name)
+		console.log(index)
+		if (index > -1){
+			active_taxa.splice(index, 1)
+			active_names.splice(index, 1)
+		}
+		console.log(active_taxa)
+		if (active_taxa.length > 0){
+			makePlot(active_taxa)
+		}
 	}
 }
